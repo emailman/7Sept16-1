42db2a69d02d0dd2983a3bd04def838170e9cecc
emailman <emailman@dtcc.edu>
2016-09-12 08:07:49 -0400

    Add project files.

---
+++ 7Sept16-1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace _7Sept16_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbxFlavors = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblFlavor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ice Cream Flavors";
+            // 
+            // lbxFlavors
+            // 
+            this.lbxFlavors.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxFlavors.FormattingEnabled = true;
+            this.lbxFlavors.ItemHeight = 20;
+            this.lbxFlavors.Location = new System.Drawing.Point(17, 36);
+            this.lbxFlavors.Name = "lbxFlavors";
+            this.lbxFlavors.Size = new System.Drawing.Size(263, 84);
+            this.lbxFlavors.TabIndex = 1;
+            this.lbxFlavors.SelectedIndexChanged += new System.EventHandler(this.lbxFlavors_SelectedIndexChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(17, 189);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 49);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblFlavor
+            // 
+            this.lblFlavor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFlavor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFlavor.Location = new System.Drawing.Point(13, 149);
+            this.lblFlavor.Name = "lblFlavor";
+            this.lblFlavor.Size = new System.Drawing.Size(267, 23);
+            this.lblFlavor.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.lblFlavor);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lbxFlavors);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cones R\' Us";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbxFlavors;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblFlavor;
+    }
+}
+
---
+++ 7Sept16-1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _7Sept16_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbxFlavors.Items.Add("vanilla");
+            lbxFlavors.Items.Add("chocolate");
+            lbxFlavors.Items.Add("strawberry");
+            lbxFlavors.Items.Add("rocky road");
+            lbxFlavors.Items.Add("fear sundae");
+            lbxFlavors.Items.Add("peach");
+        }
+
+        private void lbxFlavors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblFlavor.Text = "Enjoy your " + lbxFlavors.SelectedItem.ToString()
+                + " cone !";
+        }
+    }
+}
